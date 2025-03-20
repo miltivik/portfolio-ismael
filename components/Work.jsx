ed0f3562cbaadb61220a7b495db3c8bf2e82eed3
miltivik <134473229+miltivik@users.noreply.github.com>
2025-03-20 13:38:46 -0300

    con un proyecto real

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,40 +1,10 @@
 const works = [
     {
-      imgSrc: '/images/project-1.jpg',
-      title: 'Full stack music app',
-      tags: ['API', 'MVC', 'Development'],
-      projectLink: 'https://musify-5al0.onrender.com/'
-    },
-    {
-      imgSrc: '/images/project-2.jpg',
-      title: 'Free stock photo app',
-      tags: ['API', 'SPA'],
-      projectLink: 'https://pixstock-official.vercel.app/'
-    },
-    {
-      imgSrc: '/images/project-3.jpg',
-      title: 'Recipe app',
-      tags: ['Development', 'API'],
-      projectLink: ''
-    },
-    {
-      imgSrc: '/images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee-org/wealthome'
-    },
-    {
-      imgSrc: '/images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-    },
-    {
-      imgSrc: '/images/project-6.jpg',
-      title: 'Card Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
-    },
+      imgSrc: '/images/dashboard.png',
+      title: 'Storage Drive Dashboard',
+      tags: ['Appwrite', 'Drive', 'Development'],
+      projectLink: 'https://store-it-phi.vercel.app/home'
+    }
   ];
 import ProjectCard from "./ProjectCard";
 const Work = () => {
